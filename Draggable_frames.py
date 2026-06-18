681c2eb00a79cc2bb7230c1318707b35bf5cdd52
Saarthi Basant Gupta <saarthibasantgupta31@gmail.com>
2026-06-18 09:59:48 +0530

    Making a draggable frame

---
+++ Draggable_frames.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Draggable Sticky Note")
+root.geometry("600x400")
+
+note = tk.Frame(root, bg = "khaki", bd =1, relief="solid")
+note.place(x=50, y=50, width=100, height=100)
+
+title = tk.Label(note,
+                 text = "note 1",
+                 bg = "gold",
+                 font =("Arial", 12, "bold")
+)
+title.pack(fill="x")
+
+def start_drag(event):
+    note.drag_x = event.x_root - note.winfo_x()
+    note.drag_y = event.y_root - note.winfo_y()
+
+def drag(event):
+    x = event.x_root - note.drag_x
+    y = event.y_root - note.drag_y
+    note.place(x=x, y=y)
+
+note.bind("<Button-1>", start_drag)
+note.bind("<B1-Motion>", drag)
+
+title.bind("<Button-1>", start_drag)
+title.bind("<B1-Motion>", drag)
+
+
+root.mainloop()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,24 +1,18 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.title("Note")
-# button = tk.Button(root, text="End", width=25, command=root.destroy)
-# button.pack()
+root.title("Draggable Sticky Note")
+root.geometry("600x400")
 
-# tk.Label(root, text="First name").grid(row = 0, column=0)
-# tk.Label(root, text="Second name").grid(row = 1, column=0)
-# entry1 = tk.Entry(root)
-# entry2 = tk.Entry(root)
-# entry1.grid(row=0, column=2)
-# entry2.grid(row=1, column=2)
+note = tk.Frame(root, bg = "khaki", bd =1, relief="solid")
+note.place(x=50, y=50, width=100, height=100)
 
-menu = tk.Menu(root)
-root.config(menu=menu)
-filemenu = tk.Menu(menu)
-menu.add_cascade(label="Options", menu=filemenu)
-filemenu.add_command(label= "Add")
-filemenu.add_command(label="Edit")
-filemenu.add_command(label="Delete")
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=root.quit)
+title = tk.Lable(note,
+                 text = "note 1",
+                 bg = "gold",
+                 font =("Arial", 12, "bold")
+)
+title.pack(fill="x")
+
+
 root.mainloop()
